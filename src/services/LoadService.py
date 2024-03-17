d3475b0a88d60d41ba05ab81b49dc993615f7538
Juan Cruz Rey <juancurey2@gmail.com>
2024-03-17 01:43:44 -0300

    feature/no-ref/Creacion de la correcta gestion de constrains provenientes de los files csv, configurando de manera correcta a las primary key y Foreing Key de cada tabla, automatizando el proceso permitiendo que aun el ingreso de tablas pueda ser dinamico cumpliendo estandares. Ademas se cumple con el requerimientos de poder almacenar el dato que pide el ejercicio.

--- src/services/LoadService.py
+++ src/services/LoadService.py
@@ -1,6 +1,7 @@
 from src.resource.db.DB import DB
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKeyConstraint, PrimaryKeyConstraint
 from flask import  jsonify
 import pandas as pd
 
@@ -12,6 +13,8 @@ class LoadService():
     @staticmethod
     def loadData(files,tablesInfo):
 
+        LoadService.loadFK(tablesInfo)
+        
         if files:
             
             db = DB()
@@ -68,6 +71,58 @@ class LoadService():
             finally:
                 db.closeConnection(conn)
             return jsonify({'message': 'Data uploaded successfully'}), 200
+        else:
+            return jsonify({'error': 'No files provided'}), 400
+        
+
+    @staticmethod
+    def loadFK(tablesInfo):
+
+        if tablesInfo:   
+            try:
+                db = DB()
+                db.connect()
+                engine = db.connect()
+
+                with engine.connect() as conn:
+                    with conn.begin():
+
+                        metadata = MetaData()
+
+                        for table_data in tablesInfo:
+                            table_name = table_data["table_name"]
+                            # Creo una lista de columnas
+                            columns = []
+                            for col_name in table_data["columns"]:
+                                    if col_name.startswith("id"):  # Si la columna comienza con 'id', la definimos como Integer
+                                        columns.append(Column(col_name, Integer))
+                                    else:
+                                        columns.append(Column(col_name, String(255)))
+                            table = Table(table_name, metadata, *columns)
+                            table.append_constraint(PrimaryKeyConstraint("id"))
+                            
+                            # Definimos las claves foráneas si están presentes en el JSON
+                            if "fk" in table_data:
+                                for column_name, referenced_table in table_data["fk"].items():
+                                    try:
+                                        print(column_name, referenced_table)
+                                        table.append_constraint(
+                                            ForeignKeyConstraint([column_name], [f"{referenced_table}.id"]))
+                                    except Exception as e:
+                                            print("Error al crear FK", ":", e)
+                        # Creo las tablas en la base de datos
+                        try:
+                            print("111111111111111111111111111111111111111")
+                            metadata.create_all(engine)
+                            print("222222222222222222222222222222222222222")
+                            
+                        except Exception as e:
+                                            print("Error al crear las tablas", ":", e)
+
+            except SQLAlchemyError as e:
+                return jsonify({'error': 'Error de base de datos: {}'.format(str(e))}), 500
+            finally:
+                db.closeConnection(conn)
         else:
             return jsonify({'error': 'No files provided'}), 400
 
